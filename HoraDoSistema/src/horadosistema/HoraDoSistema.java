88a4c829a52fbf81d81ddf4730f0e33e09ef077b
David Cordeiro Moura <david.mourabr@gmail.com>
2025-08-27 11:47:26 -0300

    Create Projects

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,18 @@
+
+package horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author David Moura
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sitema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
